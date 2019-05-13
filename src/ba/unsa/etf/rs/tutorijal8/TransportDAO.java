fd25260b50fdaacdd7e5805e5af793ddc1ef037f
--unset <--unset>
2019-05-13 10:03:15 +0200

    treci

--- src/ba/unsa/etf/rs/tutorijal8/TransportDAO.java
+++ src/ba/unsa/etf/rs/tutorijal8/TransportDAO.java
@@ -1,9 +1,35 @@
 package ba.unsa.etf.rs.tutorijal8;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
 public class TransportDAO {
+    public static TransportDAO getInstance() {
+        return null;
+    }
+
     public void deleteDriver(Driver driver) {
     }
 
     public void deleteBus(Bus bus) {
+    }
+
+    public void addDriver(Driver driver) {
+    }
+
+    public ArrayList<Driver> getDrivers() {
+        return null;
+    }
+
+    public ArrayList<Bus> getBusses() {
+        return null;
+    }
+
+    public void resetDatabase() {
+
+    }
+
+    public void addBus(Bus bus) {
     }
 }
